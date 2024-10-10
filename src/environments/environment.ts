c09fc2d9adbe6b3ab553bfc3642791078da81190
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-10 22:45:42 +0500

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000', // Local API for development
+    apiUrl: 'https://server-app-chi.vercel.app', // Local API for development
   };
   
